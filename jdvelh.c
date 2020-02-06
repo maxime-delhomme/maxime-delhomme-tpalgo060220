9322b5d1bec0fa1046a825c96efa05876b6f686b
maxime-delhomme <maxdelh222@gmail.com>
2020-02-06 09:55:33 +0100

    choix par numéro

--- jdvelh.c
+++ jdvelh.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    long choix = 0;
+int main(){
+  int choix = 0;
    
-    printf (" jeu dont vous etes le heros\n\n"
-    " Acceptez-vous cette Mission ?\n\n  1.Oui\n  2.Non\n\n");
-    scanf ("%ld", &choix);
-   
-    switch(choix)
-    {
-        case 1:
             printf ("\n Que faites vous ?"
             "\n\n  1.Vous allez a gauche"
             "\n  2.Vous allez a droite\n\n");
             scanf ("%ld", &choix);
-            break;
    
-        case 2:
-            printf ("\n Au Revoir alors !!!\n\n");
-            choix = 0;
-            break;
-    }
    
-    switch(choix)
-    {
-        case 1:
+ 
+    
+		if (choix==1)
+		{
             printf ("\n Vous avez atteint le couloir de gauche");
-            break;
+        }
        
-        case 2:
+		else if (choix==2)
+		{
             printf ("\n Vous avez atteint la salle de droite");
-            break;
-    }
+        }
+    
     printf("\n");
-    system("PAUSE");
     return 0;
 }
